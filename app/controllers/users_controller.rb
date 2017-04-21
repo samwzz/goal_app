3721ea11b2aad79518c5b4c6f5a18183001b5f52
Sam Wang <sam.zz.wang@gmail.com>
2017-04-21 12:16:37 -0700

    Implement tests for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,33 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    render :index
   end
 
   def new
+    @user = User.new
+    render :new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_url(@user)
+    else
+      flash[:errors] = ['Invalid credentials']
+      render :new
+    end
   end
 
   def show
+    @user = User.find_by(id: params[:id])
+    render :show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :pasword)
+
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,23 +3,39 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
+    it "renders index" do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
     end
   end
 
   describe "GET #new" do
-    it "returns http success" do
+    it "render new form" do
       get :new
-      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:new)
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe "POST #create" do
+    context "with invalid params" do
+      it "validates the presence of user email and password" do
+        post :create, user: { username: 'user', password: "" }
+        expect(response).to render_template(:new)
+        expect(flash[:errors]).to be_present
+      end
+
+      it "validates that the password is at least 6 characters long" do
+        post :create, user: { username: 'user', password: 'haha' }
+        expect(response).to render_template(:new)
+        expect(flash[:errors]).to be_present
+      end
+    end
+
+    context "with valid params" do
+      it "redirects user to user show" do
+        post :create, user: { username: 'user', password: 'password' }
+        expect(response).to redirect_to(user_url(user))
+      end
     end
   end
 
